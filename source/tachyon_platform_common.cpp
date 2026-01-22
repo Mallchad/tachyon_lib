15af97e59712e83525bb7e64868cceb339fea8b5
Mallchad <ncaprisunfan@gmail.com>
2026-01-22 23:03:45 +0000

    Added: Logging to changing the working directory

--- source/tachyon_platform_common.cpp
+++ source/tachyon_platform_common.cpp
@@ -4,6 +4,7 @@ namespace tyon
 {
     PROC file_set_working_directory( fstring arg ) -> void
     {
+        TYON_LOGF( "Changing working directory to: '{}'", fs::canonical( arg ).string() );
         fs::current_path( arg );
     }
 
