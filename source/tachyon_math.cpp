9b3312ecc6586105b8bd255611d40bb8e7cb24e8
Erik Hall <erikhall1811@gmail.com>
2026-01-27 12:15:34 +0100

    Added; Complex number type, operators and conjugate function

--- source/tachyon_math.cpp
+++ source/tachyon_math.cpp
@@ -1,11 +1,126 @@
 
 namespace tyon
 {
+    /** Complex Numbers **/
+        // Types
+        CONSTRUCTOR c32::c32() : real(0.0f), imag(0.0f) {}
+        CONSTRUCTOR c32::c32( f32 arg ) : real(arg), imag(0.0f) {}
+        CONSTRUCTOR c32::c32( f32 real, f32 imag) : real(real), imag(imag) {}
+        CONSTRUCTOR c32::c32( f64 arg) {}
 
+        CONSTRUCTOR c64::c64() : real(0.0), imag(0.0) {}
+        CONSTRUCTOR c64::c64( f64 arg ) : real(arg), imag(0.0) {}
+        CONSTRUCTOR c64::c64( f64 real, f64 imag) : real(real), imag(imag) {}
+        CONSTRUCTOR c64::c64( f32 arg) {}
+
+        // Operators
+        // Addition
+        PROC operator+(const c32& z0, const c32& z1) -> c32
+        {
+            c32 result;
+            result.real = z0.real + z1.real;
+            result.imag = z0.imag + z1.imag;
+            return result;
+        }
+
+        PROC operator+(const c64& z0, const c64& z1) -> c64
+        {
+            c64 result;
+            result.real = z0.real + z1.real;
+            result.imag = z0.imag + z1.imag;
+            return result;
+        }
+
+        // Subtraction
+        PROC operator-(const c32& z0, const c32& z1) -> c32
+        {
+            c32 result;
+            result.real = z0.real - z1.real;
+            result.imag = z0.imag - z1.imag;
+            return result;
+        }
+
+        PROC operator-(const c64& z0, const c64& z1) -> c64
+        {
+            c64 result;
+            result.real = z0.real - z1.real;
+            result.imag = z0.imag - z1.imag;
+            return result;
+        }
+
+        // Multiplication
+        PROC operator*(const c32& z0, const c32& z1) -> c32
+        {
+            c32 result;
+            result.real = (z0.real * z1.real) - (z0.imag * z1.imag);
+            result.imag = (z0.real * z1.imag) + (z0.imag * z1.real);
+            return result;
+        }
+
+        PROC operator*(const c64& z0, const c64& z1) -> c64
+        {
+            c64 result;
+            result.real = (z0.real * z1.real) - (z0.imag * z1.imag);
+            result.imag = (z0.real * z1.imag) + (z0.imag * z1.real);
+            return result;
+        }
+
+        PROC operator*(const c32& z0, const f32& s0) -> c32
+        {
+            return c32{z0.real * s0, z0.real * s0};
+        }
+
+        PROC operator*(const c64& z0, const f64& s0) -> c64
+        {
+            return c64{z0.real * s0, z0.real * s0};
+        }
+
+        // Division
+        PROC operator/(const c32& z0, const c32& z1) -> c32
+        {
+            c32 result;
+            result.real = ((z0.real * z1.real) + (z0.imag * z1.imag)) / ((z1.real * z1.real) + (z1.imag * z1.imag));
+            result.imag = ((z0.imag * z1.real) - (z0.real * z1.imag)) / ((z1.real * z1.real) + (z1.imag * z1.imag));
+            return result;
+        }
+
+        PROC operator/(const c64& z0, const c64& z1) -> c64
+        {
+            c64 result;
+            result.real = ((z0.real * z1.real) + (z0.imag * z1.imag)) / ((z1.real * z1.real) + (z1.imag * z1.imag));
+            result.imag = ((z0.imag * z1.real) - (z0.real * z1.imag)) / ((z1.real * z1.real) + (z1.imag * z1.imag));
+            return result;
+        }
+
+        // Inverse
+        PROC operator-(const c32& z0) -> c32
+        {
+            c32 result;
+            result.real = -z0.real;
+            result.imag = z0.imag;
+            return result;
+        }
+
+        PROC operator-(const c64& z0) -> c64
+        {
+            c64 result;
+            result.real = -z0.real;
+            result.imag = z0.imag;
+            return result;
+        }
+
+        // Functions
+        PROC complex_conjugate(const c32& z) -> c32
+        {
+            return c32(z.real, -z.imag);
+        }
+
+
+
+    /** Vector 3 type **/
     CONSTRUCTOR v3_f32::v3_f32( f32 arg ) : x(arg), y(arg), z(arg) {}
     CONSTRUCTOR v3_f32::v3_f32( f32 x, f32 y, f32 z ) {}
     CONSTRUCTOR v3_f32::v3_f32( f64 arg ) {}
 
-    PROC operator*( v3_f32 arg ) -> v3_f32
-    {}
+    PROC operator*( v3_f32 arg ) -> v3_f32 {}
 }
--- source/tachyon_math.h
+++ source/tachyon_math.h
@@ -4,7 +4,64 @@
 namespace tyon
 {
 
-    /* using complex = complex_t; */
+    /** Complex Numbers */
+        // Types
+        struct c32
+        {
+            f32 real;
+            f32 imag;
+
+            CONSTRUCTOR c32();
+            CONSTRUCTOR c32( f32 arg );
+            CONSTRUCTOR c32( f32 real, f32 imag );
+            explicit CONSTRUCTOR c32( f64 arg );
+        };
+
+        struct c64
+        {
+            f64 real; 
+            f64 imag; 
+
+            CONSTRUCTOR c64();
+            CONSTRUCTOR c64( f64 arg );
+            CONSTRUCTOR c64( f64 real, f64 imag );
+            explicit CONSTRUCTOR c64( f32 arg );
+        };
+
+        // Operators
+        // Addition
+        PROC operator+(const c32& z0, const c32& z1) -> c32;
+
+        PROC operator+(const c64& z0, const c64& z1) -> c64;
+
+        // Subtraction
+        PROC operator-(const c32& z0, const c32& z1) -> c32;
+
+        PROC operator-(const c64& z0, const c64& z1) -> c64;
+
+        // Multiplication
+        PROC operator*(const c32& z0, const c32& z1) -> c32;
+
+        PROC operator*(const c64& z0, const c64& z1) -> c64;
+
+        PROC operator*(const c32& z0, const f32& s0) -> c32;
+
+        PROC operator*(const c64& z0, const f64& s0) -> c64;
+
+        // Division
+        PROC operator/(const c32& z0, const c32& z1) -> c32;
+
+        PROC operator/(const c64& z0, const c64& z1) -> c64;
+
+        // Inverse
+        PROC operator-(const c32& z0) -> c32;
+
+        PROC operator-(const c64& z0) -> c64;
+
+        // Functions
+        PROC complex_conjugate(const c32& z) -> c32;
+
+
 
     /** Simple fixed size vector type */
     struct v3_f32
