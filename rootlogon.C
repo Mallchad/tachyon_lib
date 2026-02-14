42a5b3907f6f8e122aa5904a68bcaa9641d5526d
Mallchad <ncaprisunfan@gmail.com>
2026-02-14 15:05:30 +0000

    Added: Tried to setup CERN root login script better but it doesn't really work

--- rootlogon.C
+++ rootlogon.C
@@ -1,3 +1,9 @@
 
 gInterpreter->AddIncludePath("external/fmt/include");
-gInterpreter->LoadMacro( "source/build_control/tachyon_lib_unity_core.cpp" )
+gInterpreter->LoadMacro( "source/build_control/tachyon_lib_unity_core.cpp" );
+
+#include "source/include_tachyon_lib_core.h"
+
+void rootlogon() {
+    tyon::library_context_init( new tyon::library_context );
+}
